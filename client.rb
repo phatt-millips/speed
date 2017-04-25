54f5f1f4ed4c1d5cc7bfad6d2d1de6b647d4294a
Matt Phillips <matt.phillips96@gmail.com>
2017-04-25 00:10:08 -0500

    Basic server running. Basic deck setup. No communication from client to server yet.

--- client.rb
+++ client.rb
@@ -1,1 +1,22 @@
 require 'socket'
+require 'rubycards'
+include RubyCards
+
+port = 2000 + ARGV[0].to_i
+
+s = TCPSocket.new 'localhost', port
+
+loop do 
+	puts s.gets
+	puts "press Enter when ready"
+	STDIN.gets
+	s.send 'Player 1 ready', 0
+	puts s.gets
+end
+#while line = s.gets # Read lines from socket
+	#puts line         # and print them
+	#puts "Press Enter when ready"
+	#s.write "Player 1 ready"
+#end
+
+s.close  
--- server.rb
+++ server.rb
@@ -1,1 +1,34 @@
 require 'socket'
+require 'rubycards'
+include RubyCards
+
+server1 = TCPServer.new 2001 # Server bind to port 2000
+server2 = TCPServer.new 2002 # Server bind to port 2001
+
+loop do
+	player1 = server1.accept    # Wait for a client to connect
+	player1.puts "Connected. Waiting for player 2"
+	player2 = server2.accept
+	player2.puts "Connected"
+
+	#setup
+	origin_deck = Deck.new.shuffle!
+	player1_main_deck = Hand.new.draw origin_deck 15
+	player2_main_deck = Hand.new.draw origin_deck 15
+	refresh_deck1 = Hand.new.draw origin_deck 5
+	refresh_deck2 = Hand.new.draw origin_deck 5
+	discard_card1 = Hand.new.draw origin_deck 1
+	discard_card2 = Hand.new.draw origin_deck 1
+	player1_hand = Hand.new origin_deck 5
+	player2_hand = Hand.new origin_deck 5
+
+	player1.puts "Game Ready"
+	player2.puts "Game Ready"
+	
+	player1.puts player1_hand	
+
+	p player2.gets
+		
+	player1.close
+	player2.close
+end
