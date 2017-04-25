c7832333479c16db9ffb3151357da89bcd776c5c
Matt Phillips <matt.phillips96@gmail.com>
2017-04-25 13:26:04 -0500

    Hand-shake established

--- client.rb
+++ client.rb
@@ -5,14 +5,22 @@ include RubyCards
 port = 2000 + ARGV[0].to_i
 
 s = TCPSocket.new 'localhost', port
+puts s.gets
 
-loop do 
-	puts s.gets
-	puts "press Enter when ready"
-	STDIN.gets
-	s.send 'Player 1 ready', 0
-	puts s.gets
+#waits for reponse from server
+while (s.gets.chomp != 'Server Ready') 
+	system('cls')
+	puts 'Waiting on server'
 end
+
+puts "Press Enter when ready"
+STDIN.gets 
+s.send 'Begin', 0
+
+#while keystroke != 'q' 
+	#puts s.gets	
+	#keystroke = STDIN.gets
+#end
 #while line = s.gets # Read lines from socket
 	#puts line         # and print them
 	#puts "Press Enter when ready"
--- server.rb
+++ server.rb
@@ -2,25 +2,36 @@ require 'socket'
 require 'rubycards'
 include RubyCards
 
+
 server1 = TCPServer.new 2001 # Server bind to port 2000
 server2 = TCPServer.new 2002 # Server bind to port 2001
+player1 = server1.accept    # Wait for a client to connect
+player1.puts "Connected. Waiting for player 2"
+player2 = server2.accept
+player2.puts "Connected"
+puts "Both Players Connected"
+puts "Server Ready"
+player1.puts "Server Ready"
+player2.puts "Server Ready"
+while player1.recv(10).chomp != "Player Ready" || player2.recv(10).chomp != "Player Ready" do
+	puts 'waiting for players'	
+end
+
+player1.puts "Begin"
+player2.puts "Begin"
 
 loop do
-	player1 = server1.accept    # Wait for a client to connect
-	player1.puts "Connected. Waiting for player 2"
-	player2 = server2.accept
-	player2.puts "Connected"
-
+	
 	#setup
 	origin_deck = Deck.new.shuffle!
-	player1_main_deck = Hand.new.draw origin_deck 15
-	player2_main_deck = Hand.new.draw origin_deck 15
-	refresh_deck1 = Hand.new.draw origin_deck 5
-	refresh_deck2 = Hand.new.draw origin_deck 5
-	discard_card1 = Hand.new.draw origin_deck 1
-	discard_card2 = Hand.new.draw origin_deck 1
-	player1_hand = Hand.new origin_deck 5
-	player2_hand = Hand.new origin_deck 5
+	player1_main_deck = Hand.new.draw origin_deck,15
+	player2_main_deck = Hand.new.draw origin_deck,15
+	refresh_deck1 = Hand.new.draw origin_deck,5
+	refresh_deck2 = Hand.new.draw origin_deck,5
+	discard_card1 = Hand.new.draw origin_deck,1
+	discard_card2 = Hand.new.draw origin_deck,1
+	player1_hand = Hand.new.draw origin_deck,5
+	player2_hand = Hand.new.draw origin_deck,5
 
 	player1.puts "Game Ready"
 	player2.puts "Game Ready"
@@ -32,3 +43,4 @@ loop do
 	player1.close
 	player2.close
 end
+
