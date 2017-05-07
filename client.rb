8925f28ed8ed9fe18a7d2263e3b9d8c4e6b857ce
Matt Phillips <matt.phillips96@gmail.com>
2017-05-06 20:49:18 -0500

    Server improved. Now allows for multithreaded listening which stops listening when a move is recieved from a player. Client still needs improvement. Currently moving to a more object oriented style.

--- client.rb
+++ client.rb
@@ -1,27 +1,80 @@
 require 'socket'
 require 'rubycards'
 include RubyCards
-system('cls')
-
-hand = Hand.new
-s = TCPSocket.new 'localhost', 2000 
-while msg = s.gets do
-	puts msg 
-	break if  msg.chomp == "~~"
+class TCPSocket 
+	def listen(print = false, stop_msg = "~~")
+		ret_array = []
+		
+		while ((msg = self.gets.chomp) != stop_msg) do
+			if print
+				STDOUT.puts msg
+			end
+			ret_array << msg
+		end
+		ret_array
+	end
 end
+class Player 
+	def initialize(port)
+		@s = TCPSocket.new 'localhost', port
+		@hand = Hand.new
+		s.listen(true)
+		puts "Press Enter when ready"
+		STDIN.gets
+		s.puts 'Player Ready'
+	end
 
-puts "Press Enter when ready"
+	def get_cards
+
+	end
+
+	def get_discard
+
+	end
+	
+	def get_total
+
+	end
+
+	def reqest_new_card
+
+	end
+	
+	def request_new_discard
+	
+	end
+
+	def discard(card,pile)
+
+	end
+
+	def listen_threaded
+
+	end
+	
+end
+system('cls')
+hand = Hand.new
+puts "Welcome to Speed"
+s = TCPSocket.new 'localhost', 2000 
+#Intro Message
+s.listen true
+
 STDIN.gets
-s.send 'Player Ready', 0
+s.puts 'Player Ready'
 
-msg = s.gets.chomp.split(":")
-while msg[0] != "~~"
-	msg[0] = msg[0].to_i == 0? msg[0] : msg[0].to_i 	
-	hand.add(Card.new(msg[0], msg[1]))
-	msg = s.gets.chomp.split(":")
+discards = s.listen.map{|discard| discard.split(":")}
+puts discards.shift
+
+cards = s.listen.map{|card| card.split(":")}
+puts cards.shift
+cards.each do |card|
+	card[0] = card[0].to_i == 0? card[0] : card[0].to_i 	
+	hand.add(Card.new(card[0], card[1]))
 end	
 hand.sort!
 puts hand
+s.puts STDIN.gets
 #puts s.gets
 #keystroke = ""
 #while keystroke != 'q' 
--- server.rb
+++ server.rb
@@ -2,44 +2,159 @@ require 'socket'
 require 'rubycards'
 include RubyCards
 #setup 
+class Player 
+	@@num_players = 0
+	def initialize(deck, server)
+		@hand = Hand.new.draw deck, 5
+		@draw_pile = Hand.new.draw deck, 15
+		@port = server.accept
+		@@num_players += 1
+		@player_id = @@num_players 
+	end
+	def send_current_cards
+		self.send("Hand")
+		@hand.each {|card| self.send(card.rank + ":" + card.suit)}
+		self.end_msg
+	end
+	def send(string)
+		STDOUT.puts "#{self} --> #{string}"
+		@port.puts string
+	end
+	def gets
+		ret_val = @port.gets.chomp	
+		STDOUT.puts "#{self} <-- #{ret_val}"
+		ret_val
+	end
+	def end_msg
+		self.send '~~'	
+	end
+	def num_cards
+		@hand.count + @draw_pile.count
+	end
+	def to_s
+		"Player #{@player_id}"
+	end
+end
 
-origin_deck = Deck.new.shuffle!
-player1_main_deck = Hand.new.draw origin_deck,15
-player2_main_deck = Hand.new.draw origin_deck,15
-refresh_deck1 = Hand.new.draw origin_deck,5
-refresh_deck2 = Hand.new.draw origin_deck,5
-discard_card1 = Hand.new.draw origin_deck,1
-discard_card2 = Hand.new.draw origin_deck,1
-player1_hand = Hand.new.draw origin_deck,5
-player2_hand = Hand.new.draw origin_deck,5
+class Game
+	def initialize(port = 2000)
+		@server = TCPServer.open(port)
+		@deck = Deck.new.shuffle!
+		@refresh1 = Hand.new.draw @deck,5
+		@refresh2 = Hand.new.draw @deck,5
+		@discard1 = @deck.draw
+		@discard2 = @deck.draw
+		STDOUT.puts("Server on port #{port}")
+		@p1 = Player.new(@deck, @server)
+		STDOUT.puts("Player 1 Connected")
+		@p1.send "Connected to server\nWaiting on opponent"
+		@p2 = Player.new(@deck, @server)
+		STDOUT.puts("Player 2 Connected")
+		@p2.send "Connected to server"	
+		self.to_both_players "Both players connected"
+	end
+	def to_both_players(string, ending_msg = true)
+		@p1.send(string)
+		@p1.end_msg if ending_msg
+		@p2.send(string)
+		@p2.end_msg if ending_msg
+	end
+	def listen(player = 0)
+		case player
+		when 0
+			[@p1.gets,@p2.gets]	
+		when 1
+			@p1.gets
+		when 2
+			@p2.gets
+		end
+	end
+	def listen_threaded 
+		STDOUT.puts "Server Listening"
+
+		p1t = Thread.new do
+			[@p1,@p1.gets]
+		end
+		p2t = Thread.new do
+			[@p2,@p2.gets]
+		end
+
+		#Stops listening to one player when the other starts talking
+		control_thread = Thread.new do
+			ret_thread = []
+			loop do
+				if (!p1t.alive?) 
+					ret_thread = p1t
+					p2t.kill
+					break
+				elsif (!p2t.alive?)
+					ret_thread = p2t
+					p1t.kill
+					break
+				end
+			end
+			ret_thread
+		end
+		control_thread.join
+		return control_thread.value
+	end
+	def send_cards(player = 0)
+		case player
+		when 0
+			@p1.send_current_cards
+			@p2.send_current_cards
+		when 1
+			@p1.send_current_cards
+		when 2 
+			@p2.send_current_cards
+		end
+	end
+	def send_discard
+		to_both_players("Discard", false)
+		to_both_players("#{@discard1.rank}:#{@discard1.suit}", false)
+		to_both_players("#{@discard2.rank}:#{@discard2.suit}")
+	end
+		
+end
+game = Game.new
+game.listen #listens for any responce
+game.send_discard
+game.send_cards
+game.listen_threaded
+
+#origin_deck = Deck.new.shuffle!
+#player1_main_deck = Hand.new.draw origin_deck,15
+#player2_main_deck = Hand.new.draw origin_deck,15
+#refresh_deck2 = Hand.new.draw origin_deck,5
+#discard_card2 = Hand.new.draw origin_deck,1
+##player1_hand = Hand.new.draw origin_deck,5
+##player2_hand = Hand.new.draw origin_deck,5
 
 #Accept
-system('cls')
-puts "Server Running"
-server = TCPServer.open(2000)
-puts "player 1 opened"
-Thread.fork(p1 = server.accept) do |player1|
-	puts "player 1 accepted"
-	player1.puts "Hello player 1" 
-	player1.puts "Waiting on player 2"
-end
-puts "player 2 opened"
-Thread.fork(p2 = server.accept) do |player2|
-	puts "player 2 accepted"
-	player2.puts "Hello player 2"	
-end
-p1.puts "Both Players Connected"
-p2.puts "Both Players Connected"
-p1.puts "~~"
-p2.puts "~~"
+##system('cls')
+##puts "Server Running"
+##server = TCPServer.open(2000)
+##puts "player 1 opened"
+##p1 = server.accept
+##puts "player 1 accepted"
+##p1.puts "Hello player 1" 
+##p1.puts "Waiting on player 2"
+##puts "player 2 opened"
+##p2 = server.accept
+##puts "player 2 accepted"
+##p2.puts "Hello player 2"	
+##p1.puts "Both Players Connected"
+##p2.puts "Both Players Connected"
+##p1.puts "~~"
+##p2.puts "~~"
 
 #Begin Game
-p1.recv(12) && p2.recv(12) #waits until both players are ready
-puts "Game Begin"
-player1_hand.each {|card| p1.puts card.rank + ':' + card.suit }
-player2_hand.each {|card| p2.puts card.rank + ':' + card.suit }
-p1.puts "~~"
-p2.puts "~~"
+##puts p1.recv(12) && p2.recv(12) #waits until both players are ready
+##puts "Game Begin"
+##player1_hand.each {|card| p1.puts card.rank + ':' + card.suit }
+##player2_hand.each {|card| p2.puts card.rank + ':' + card.suit }
+##p1.puts "~~"
+##p2.puts "~~"
 
 #OLD SERVER
 #server2 = TCPServer.new 2002 # Server bind to port 2001
